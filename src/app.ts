247a502cdc7fe9e582afb0931458d33c8fd99a2b
Junieslon <juniosantos_@hotmail.com>
2026-02-09 15:20:37 -0400

    Refactor cron job implementation by removing deprecated routes and scheduling presence services directly in app.ts. Updated Vercel configuration to reflect these changes.

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import availabilityRoutes from "./routes/availability.routes";
 import handleError from "./errors/handleError";
 import whatsappRoutes from "./routes/whatsapp.routes";
 import { sendPresenceServices } from "./services/presence/sendPresence.services";
-import cronRoutes from "./routes/cron.routes";
+
 
 const app: Application = express();
 app.use(cors());
@@ -15,8 +15,18 @@ app.use(express.json());
 app.use("/appointments", appointmentsRoutes);
 app.use("/availability", availabilityRoutes);
 app.use("/whatsapp", whatsappRoutes)
-app.use("/api", cronRoutes);
 
 app.use(handleError);
+
+// 🔹 CRON JOB - roda todos os dias às 5h
+cron.schedule("25 19 * * *", async () => {
+    try {
+        // Chamando a função diretamente
+        // await sendPresenceServices();
+        console.log("sendPresenceServices executada com sucesso!");
+    } catch (err) {
+        console.error("Erro ao chamar a rota:", err);
+    }
+});
 
 export default app;
