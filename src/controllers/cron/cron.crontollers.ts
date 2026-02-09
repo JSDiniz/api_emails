69e6ce68395f9c64d769cc1db3506644f9014348
Junieslon <juniosantos_@hotmail.com>
2026-02-09 15:47:19 -0400

    Fix log message formatting in cron controller for clarity

--- src/controllers/cron/cron.crontollers.ts
+++ src/controllers/cron/cron.crontollers.ts
@@ -4,7 +4,8 @@ import { Request, Response } from "express";
 const cronController = async (req: Request, res: Response) => {
     try {
 
-        console.log("[CRON] sendPresenceServices executada!");
+        console.log("[CRON] send Presence Services executada!");
+
         res.status(200).send("Função executada!");
     } catch (err) {
         console.error("[CRON] Erro:", err);
