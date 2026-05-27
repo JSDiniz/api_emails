fcd201c68bf7bfca1b55c64b7e46f770bde0e215
Junieslon <juniosantos_@hotmail.com>
2026-05-27 00:17:44 -0400

    fix: Correct date formats in doctor availability mock for June 2026

--- src/mocks/doctorAvailability.mock.ts
+++ src/mocks/doctorAvailability.mock.ts
@@ -49,10 +49,10 @@ export const doctorAvailabilityMock: DoctorAvailability[] = [
       { date: "2026-05-30", periods: [{ start: "13:00", end: "17:00" }] },
 
       // ===== JUNHO 2026 =====
-      { date: "2026-20-02", periods: [{ start: "13:00", end: "17:00" }] },
-      { date: "2026-23-12", periods: [{ start: "08:00", end: "12:00" }] },
-      { date: "2026-27-23", periods: [{ start: "13:00", end: "17:00" }] },
-      { date: "2026-30-26", periods: [{ start: "08:00", end: "12:00" }] },
+      { date: "2026-06-20", periods: [{ start: "13:00", end: "17:00" }] },
+      { date: "2026-06-23", periods: [{ start: "08:00", end: "12:00" }] },
+      { date: "2026-06-27", periods: [{ start: "13:00", end: "17:00" }] },
+      { date: "2026-06-30", periods: [{ start: "08:00", end: "12:00" }] },
     ],
   },
   {
